2eb5748bafac2ba6f3dfc69d05438e05e9024dca
Olaiya Segun <vadeshayo@gmail.com>
2023-07-16 14:18:38 +0100

    Added Options

--- index.js
+++ index.js
@@ -44,10 +44,8 @@ module.exports.handler = async (event) => {
 	return {
 		statusCode: 200,
 		headers: {
-			'Access-Control-Allow-Origin': 'origin',
-			'Control-Allow-Origin': '*',
-			'Access-Control-Allow-Headers': 'Content-Type',
-			'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
+			'Access-Control-Allow-Origin': '*',
+			'Access-Control-Allow-Headers': '*'
 		},
 		body: JSON.stringify(
 		  {
